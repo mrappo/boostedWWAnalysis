71209a131cbe0028466ce34945aa0f883c164bbd
mrappo <m.rappo@campus.unimib.it>
2017-02-18 11:07:44 +0100

    run all for FullCLs limit

---
+++ MATTEO_run_Total_FullCLs.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+counter=0
+file="DirectoryForFullCLs.txt"
+while IFS= read -r line
+do
+        # display $line or do somthing with $line
+	printf '%f\n' "$counter"
+	printf '%s\n' "$line"
+	DirVector[$counter]=$line
+	echo ${DirVector[$counter]}
+	((counter++))
+	
+done <"$file"
+
+#echo ${DirVector[5]}
+
+
+getBack='../../../../../../../../../'
+#cd ${DirVector[5]}
+
+#pwd
+
+
+#cd $getBack
+#ls
+#pwd
+
+for i in `seq 0 $((counter-1))`; do
+            printf '\n'
+            echo item: $i
+            cd ${DirVector[$i]}
+            pwd
+            
+            python MATTEO_runLocal_fullCLs.py --batchMode
+            cd $getBack
+        done
